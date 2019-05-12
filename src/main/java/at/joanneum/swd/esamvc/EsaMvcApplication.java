f3d033f1b28c2fe705602ac6b90b54e89bf39767
Ricardo Zanini <zanini@redhat.com>
2019-05-12 17:18:57 -0300

    converting to war deployment

--- src/main/java/at/joanneum/swd/esamvc/EsaMvcApplication.java
+++ src/main/java/at/joanneum/swd/esamvc/EsaMvcApplication.java
@@ -2,11 +2,18 @@ package at.joanneum.swd.esamvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EsaMvcApplication {
+public class EsaMvcApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EsaMvcApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EsaMvcApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsaMvcApplication.class, args);
+    }
 }
